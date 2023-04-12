3a8f844482e047a77a782a9eb6fea36b50e32ca4
Moises Moreno Castro <60885844+SoyMoikas@users.noreply.github.com>
2023-04-12 13:40:28 -0700

    navchnge

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 //import '../assets/css/Navbar.css'
 //import '../assets/css/Root.css'
+import '../assets/css/NavbarStyle.css'
+import '../assets/js/NavbarAsset.js'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
