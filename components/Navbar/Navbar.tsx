0e6b3976addc282012c8860cd581c4fc201d96f9
Javier flores <javier.flores@ivitech.com.mx>
2023-04-18 17:55:38 -0600

    v0.12.19 se mejora navbar pero aun no se termina

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,39 @@
 import React, { useContext } from 'react'
 import Link from 'next/link'
+  
+
 
 const Navbar = () => {
   return (
-    <h1>hola</h1>
+    <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+        <div className="navbar-header">
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+        </button>
+        <Link className="navbar-brand--icon" href="/"></Link>
+        <div className="navbar-brand">Beestattoos_</div>
+        </div>
+        <div className="collapse navbar-collapse" id="myNavbar">
+        <ul className="nav navbar-nav">
+            <li><Link href="/">Inicio</Link></li>
+            <li><Link href="/galeria">Galeria</Link></li>
+            <li><Link href="/bocetos">Diseños</Link></li>
+            <li><Link href="/citas">Citas</Link></li>
+            <li><Link href="/nosotros">Nosotros</Link></li>
+            <li><Link href="/contacto">Contacto</Link></li>
+
+
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+            <li><a href="/login"><span className="glyphicon glyphicon-user"></span></a></li>
+            <li><a href="/"><span className="glyphicon glyphicon-log-in"></span></a></li>
+        </ul>
+        </div>
+    </div>
+    </nav>
     )
 }
 
