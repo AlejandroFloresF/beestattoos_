eb4ba8d7832db9ba4df9e101bd3d22da2e5ed1a4
Javier flores <javier.flores@ivitech.com.mx>
2023-04-18 16:11:04 -0600

    v0.12.18 footer finalizado.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -3,35 +3,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-        <nav className="navbar navbar-inverse">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" href="/nosotros">Beestattoos_</Link>
-                </div>
-                <ul className="nav navbar-nav">
-                    <li>
-                        <Link href="/">Inicio</Link>
-                    </li>
-                    <li>
-                        <Link href="/galeria">Galeria</Link>
-                    </li>
-                    <li>
-                        <Link href="/bocetos">Diseños</Link>
-                    </li>
-                    <li><Link href="/contacto">Contacto</Link></li>
-                    <li><Link href="/citas">Citas</Link></li>
-                </ul>
-                <ul className="nav navbar-nav navbar-right">
-                <li className="dropdown">
-                        <Link className="dropdown-toggle" data-toggle="dropdown" href="#">Identificarse<span className="caret"></span></Link>
-                        <ul className="dropdown-menu">
-                            <li><Link href="/login">Iniciar Sesion</Link></li>
-                            <li><Link href="/sing in">Registrarse</Link></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <h1>hola</h1>
     )
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../assets/css/NavbarStyle.css'
 import '../assets/css/GaleriaStyle.css'
 import '../assets/css/FooterStyle.css'
-import '../assets/js/NavbarScript.js'
+import '../assets/css/RootStyle.css'
+
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
