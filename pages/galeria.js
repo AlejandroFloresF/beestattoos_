4764e3b0611f68587bbb2e07b945f00221ff762d
Javier flores <javier.flores@ivitech.com.mx>
2023-04-22 10:57:08 -0600

    v017.25 Fix en galeria/portafolio imagenes no se repiten como antes

--- pages/galeria.js
+++ pages/galeria.js
@@ -6,45 +6,50 @@ import Footer from "../components/Footer"
 
 
 const Galeria = () => {
+
+    const Focus = ()=> {
+        var picture = getElementbyid
+    }
+
     return (
         <div className="main-galery-container">
             <Navbar/>
                 <section className="main-galery-container--slider">
                     <ul className="photos-container--slider">
-                        <li className="photos-container--card">
+                        <li onClick={Focus} id="photos-container--card" className="photos-container--card">
                             
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
-                        <li className="photos-container--card">
+                        <li id="photos-container--card" className="photos-container--card">
 
                         </li>
                     </ul>
